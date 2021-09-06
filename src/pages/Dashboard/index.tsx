b029c41325fe6142f39f74a0dacd4c5a0891f006
Cassio <cassiocastro222@gmail.com>
2021-09-06 14:23:09 -0300

    Estilização da tela principal e confecção dos cards principais

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,45 @@
 import React from "react";
-import { Container, Title } from './styles';
+import { HighlightCard } from "../../Components/HighlightCard";
+import { 
+    Container,
+     Header,
+     UserWrapper,
+     UserInfo,
+     Photo,
+     User,
+     UserGreetings,
+     UserName,
+     Icon,
+     HighlightCards
+} from './styles';
 
 
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>
-                Dashboard
-            </Title>
+            <Header>
+                <UserWrapper >
+                    <UserInfo>
+                        <Photo source = {{ uri: 'https://avatars.githubusercontent.com/u/56850671?v=4' }}/>
+                        <User>
+                            <UserGreetings>
+                                Olá,
+                            </UserGreetings>
+                            <UserName>
+                                Cássio
+                            </UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name = "power"/>
+                </UserWrapper>
+            </Header>
+            
+            <HighlightCards>
+                <HighlightCard/>
+                <HighlightCard/>
+                <HighlightCard/>
+            </HighlightCards>
         </Container>
     )
 }
